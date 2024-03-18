ae335f42f6e4296c3ad60b276d17f757b7bd11fb
Jacktwison <jacktwison@gmail.com.com>
2024-03-18 17:03:52 +0200

    Data

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM php:7.2-apache
+RUN docker-php-ext-install mysqli
+WORKDIR /var/www/html
+RUN apt-get update -y && apt-get install -y libmariadb-dev
---
+++ userRegistration.php
@@ -0,0 +1,17 @@
+<html>
+    <head><title>I CODE LTd</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</head>
+<body bgcolor="Gold"><center>
+    <form action="" method="POST">
+        <table border="0">
+            <tr><td colspan="2"><center><h1><b>User Registration Form</b></center></td></tr>
+            <tr><td><label>Names</label></td><td><input type="text" name="name" placeholder="Enter your Names"></td></tr>
+            <tr><td><label>Names</label></td><td><input type="email" name="email" placeholder="Enter your email"></td></tr>
+            <tr><td><label>Names</label></td><td><input type="password" name="password" placeholder="Enter your password"></td></tr>
+            <tr><td colspan="2"><center><h1><b><input type="submit" name="btn" value="Submit"></b></center></td></tr>
+</table>
+</Form></center>
+</body>
+</html>
